b9380cfa679051ec8e0c2e2520a6b113eafe1b7e
echo__off <ghaschel@gmail.com>
2018-03-01 22:37:32 -0300

    added working initial code for windows

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const child_process = require('child_process');
-var asdf;
-
-try {
-    asdf = child_process.spawn('/bin/bash', [__dirname + '/scripts/linux-call-uha.sh', '&']);
-    // asdf.stderr.pipe(process.stderr);
-    asdf.stdout.pipe(process.stdout);
-    asdf.stdin.pipe(process.stdin);
-  } catch (ex) {
-      console.log(ex);
-    asdf = ex.stdout;
-  }
-
-  //console.log(asdf);
-
-// child_process.execSync('wine', ['./bin/uharc.exe', 'a', '-d2', '-m3', '-md32768', '-mm+', '-y+', './u.uha', './bin/*.exe']).on('exit', function(code, signal) {
-//     console.log(code);
-//     console.log(signal);
-// }).on('error', function(code, signal) {
-//     console.log(code);
-//     console.log(signal);
-// });
-// child_process.execSync('./scripts/linux-call-uha.sh');
+  const { spawn } = require('child_process');
+  const child = spawn('./bin/uharc.exe', ['a', '-d2', '-m3', '-md32768', '-mm+', '-y+', 'u.uha', './bin/*.exe']);
+ 
+  child.stdout.setEncoding('utf8');
+  child.stdout.on('data', (chunk) => {
+    console.log(chunk)
+    // data from standard output is here as buffers
+  });
+  
+  // since these are streams, you can pipe them elsewhere
+  child.stderr.pipe(process.stderr);
+  
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
